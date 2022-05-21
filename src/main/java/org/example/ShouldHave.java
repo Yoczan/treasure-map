efe546af808977b1eb5a1971aefe4a884fd47eb3
Yohan Kulczak <yohan.kulczak@gmail.com>
2022-05-21 16:00:53 +0200

    Make should have with adventurers

--- src/main/java/org/example/ShouldHave.java
+++ src/main/java/org/example/ShouldHave.java
@@ -1,28 +1,36 @@
 package org.example;
 
+import domain.Adventurer;
 import domain.Content;
 import domain.Map;
 import domain.Mountain;
+import domain.Movement;
+import domain.Orientation;
 import domain.Position;
 import domain.Treasures;
 
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShouldHave {
 
-    public static void main(String[] args) {
-        List<String> lines = Files.readAllLines();
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.getProperty("user.dir"));
+        List<String> mapSpecifications = Files.readAllLines(Paths.get("src/main/resources/map.txt"));
+        List<String> adventurers = Files.readAllLines(Paths.get("src/main/resources/adventurers.txt"));
 
         int width = 0;
         int height = 0;
         java.util.Map<Content, Position> contentAndPositions = new HashMap<>();
 
-        for (String line : lines) {
+        for (String line : mapSpecifications) {
             String[] parameters = line.split(" ");
-
             switch (parameters[0]) {
                 case "C" -> {
                     width = Integer.parseInt(parameters[1]);
@@ -41,14 +49,28 @@ public class ShouldHave {
                 }
             }
         }
-
         Map map = new Map(width, height);
-        map.populate(new Position(3, 2), new Treasures(1));
-        map.populate(new Position(1, 3), new Treasures(3));
-        map.populate(new Position(5, 3), new Mountain());
+
+        List<AdventurerMoving> adventurersMoves = new ArrayList<>();
+        for (String adventurer : adventurers) {
+            String[] parameters = adventurer.split(" ");
+            Orientation orientation = Orientation.valueOf(parameters[2]);
+            String[] positions = parameters[1].split("-");
+            Position position = new Position(Integer.parseInt(positions[0]), Integer.parseInt(positions[1]));
+            Adventurer key = new Adventurer(orientation, position, map, parameters[0]);
+            contentAndPositions.put(key, position);
+            List<Movement> moves = Arrays.stream(parameters[3].split("")).map(Movement::valueOf).collect(Collectors.toList());
+            adventurersMoves.add(new AdventurerMoving(key, moves));
+        }
 
         for (java.util.Map.Entry<Content, Position> contentPositionEntry : contentAndPositions.entrySet()) {
-            map.populate(contentPositionEntry.getValue(), contentPositionEntry.getKey());
+            Position position = contentPositionEntry.getValue();
+            Content content = contentPositionEntry.getKey();
+            map.populate(position, content);
+        }
+
+        for (AdventurerMoving adventurersMove : adventurersMoves) {
+            new Thread(adventurersMove).start();
         }
     }
 }
